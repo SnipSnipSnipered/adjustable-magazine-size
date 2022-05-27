25acdbc1b86eb05eb9a635ae1d80fb1b977940c2
SnipSnipSnipered <99442665+SnipSnipSnipered@users.noreply.github.com>
2022-05-27 21:01:00 +0200

    Initial commit for 0.1.0

---
+++ adjustable-magazine-size_0.1.0/prototypes/magazines.lua
@@ -0,0 +1,5 @@
+data.raw["ammo"]["firearm-magazine"].magazine_size = settings.startup["firearm-magazine-size"].value 
+
+data.raw["ammo"]["piercing-rounds-magazine"].magazine_size = settings.startup["piercing-rounds-magazine-size"].value
+
+data.raw["ammo"]["uranium-rounds-magazine"].magazine_size = settings.startup["uranium-rounds-magazine-size"].value 
---
+++ adjustable-magazine-size_0.1.0/settings.lua
@@ -0,0 +1,26 @@
+data:extend({
+    {
+        type = "int-setting",
+        name = "firearm-magazine-size",
+        setting_type = "startup",
+        minimum_value = 5,
+        default_value = 30,
+        maximum_value = 100
+    },
+    {
+        type = "int-setting",
+        name = "piercing-rounds-magazine-size",
+        setting_type = "startup",
+        minimum_value = 5,
+        default_value = 30,
+        maximum_value = 100
+    },
+    {
+        type = "int-setting",
+        name = "uranium-rounds-magazine-size",
+        setting_type = "startup",
+        minimum_value = 5,
+        default_value = 30,
+        maximum_value = 100
+    }
+})
